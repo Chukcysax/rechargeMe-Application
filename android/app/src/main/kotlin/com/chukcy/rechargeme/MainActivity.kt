2899ef31aa3b96b2386eddc58fcc57a2062db25a
Innocent Chukcy <nwabuakuinnocent@gmail.com>
2022-10-27 03:10:46 +0100

    Just started project by creating different screens i'll be needing for the job

---
+++ android/app/src/main/kotlin/com/chukcy/rechargeme/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chukcy.rechargeme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePageScreen extends StatefulWidget {
+  const HomePageScreen({super.key});
+
+  @override
+  State<HomePageScreen> createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends State<HomePageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Home"),
+      ),
+    );
+  }
+}
